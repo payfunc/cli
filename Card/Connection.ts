54e66ad53d724efa9465c859c371bab542cd1d21
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-09-28 10:48:06 +0200

    FIX: fixed cloud old-account-test (#23)
    * fixed cloud old-account-test
    
    * fixed

--- Card/Connection.ts
+++ Card/Connection.ts
@@ -1,12 +1,7 @@
-import * as authly from "authly"
 import * as cardfunc from "@cardfunc/cli"
 import * as payfunc from "@payfunc/model"
 
 export async function convert(connection: cardfunc.Connection): Promise<cardfunc.Connection | undefined> {
-	const payfuncPublic = await authly.Verifier.create("public").verify(connection?.credentials?.keys.public)
-	const cardfuncKey =
-		payfunc.Merchant.V1.Key.is(payfuncPublic) &&
-		typeof payfuncPublic.option.card == "string" &&
-		(await authly.Verifier.create("public").verify(payfuncPublic.option.card))
-	return (cardfuncKey && (await connection.change({ url: cardfuncKey.iss }))) || undefined
+	const payfuncPublic = await payfunc.Merchant.Key.KeyInfo.unpack(connection?.credentials?.keys.public, "public")
+	return (payfuncPublic && (await connection.change({ url: payfuncPublic?.card?.url }))) || undefined
 }
--- Test/card-old-account.ts
+++ Test/card-old-account.ts
@@ -1,11 +1,8 @@
 import * as authly from "authly"
-import * as gracely from "gracely"
 import * as paramly from "paramly"
 import * as cardfunc from "@cardfunc/cli"
 import * as payfunc from "@payfunc/model"
-import * as Account from "../Account"
 import * as Order from "../Order"
-import * as Card from "../Card"
 
 export namespace cardOldAccount {
 	export const command: paramly.Command<cardfunc.Connection> = {
@@ -14,52 +11,17 @@ export namespace cardOldAccount {
 			"Creates a card order with an old account (with reference to a @cardfunc/model.Account), charges and refunds it.",
 		examples: [],
 		execute: async (connection, argument, flags) => {
-			const cardfuncConnecton = connection ? await Card.Connection.convert(connection) : undefined
-			const authorization =
-				cardfuncConnecton &&
-				(await cardfunc.Authorization.create(
-					cardfuncConnecton,
-					{
-						number: "cardfunc-account-create-test",
-						account: "create",
-						amount: 0,
-						currency: "SEK",
-						card: {
-							pan: "4111111111111111",
-							expires: [9, 99],
-							csc: "987",
-						},
-					},
-					true
-				))
-			const unpacked =
-				authorization && !gracely.Error.is(authorization)
-					? await cardfunc.Authorization.verify(authorization)
-					: undefined
-			const account =
-				connection && unpacked && (unpacked.card.account || unpacked.card.reference)
-					? await Account.create(connection, {
-							method: [
-								{
-									type: "card",
-									reference: unpacked.card.account ?? unpacked.card.reference ?? "",
-									scheme: "visa",
-									iin: "411111",
-									last4: "1111",
-									expires: [9, 99],
-								},
-							],
-					  })
-					: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwYXlmdW5jIiwiaWF0IjoxNjAwNzc5NTg4NzM2LCJhdWQiOiJwcm9kdWN0aW9uIiwicmVmZXJlbmNlIjoiZXlKaGJHY2lPaUpTVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKRFlYSmtSblZ1WXlJc0ltbGhkQ0k2TVRZd01EYzNNemt6TnpjM01Dd2lZWFZrSWpvaWNISnZaSFZqZEdsdmJpSXNJbWxrSWpvaWQweFRWSHBGY21FaWZRLlRNNm53c3c2ckp6aVpFMEVWZ0xzZlJNbFNqZ1BCMmZOcDlySUVLbUw5YU1oWHZ2bFR2X0J2T3NhQlFtMzVndHk4WEU2MGphWjNzQW9GUjNCVUI3dC1BeXY3LXV5WE91VU54YUF5V21IUVZWTC1BdnJFWkJBOEFISXRsUkJhOU9TdzZhd3pVYjZkaFExVkx4Z0d5RVdrQ2dHMjZNSm8xTzlKYzd2a1FhOVg0b3Bveml4UUdZS1RCOFJHam9PYlhKY1B0ektQb0xoUE5CNlZpNFBvaEFQMEdEMnpyZ19uY2t6U2FmZHAzRWNDa1dfMXNoMHVEWG05TWNsN3BQallNNG1meDFwSTR0NDB0MzVBMFVEYlVNRXJPcTUwVXBpV0hYcWFsWE9rOEV1akNTTGpkNnpkZlpRUXozeERhbGE0R045WXpidlhTajZTMkVKTWNJY2pHZTctQSIsInR5cGUiOiJjYXJkIiwic2NoZW1lIjoidmlzYSIsImlpbiI6IjQxMTExMSIsImxhc3Q0IjoiMTExMSIsImV4cGlyZXMiOlsyLDIyXSwiY3JlYXRlZCI6IjIwMjAtMDktMjJUMTI6NTk6NDguNjY5WiJ9.q0urLPlkr84tLvBJsPze2Nb1mLcJ6qgOHrNBXveFZlXDWeKTU0XoxTN5uRBWWq8RvCysCiVTaBVznPfpKvrNQGLPGBxRo0y-xEx2ZLTMuJnMsqco-tNA8seO9HwAY5QOapIcSAlZqcNvHrEOWg62hXhrnUWZgAmgVrpgg5bQtp_ZH5SVdZdazB-1fZAMpChJkkyDJLDhO-T7ilNpDHV-AeUYs-WH6ulITIqGWF1tse4bRMYtoAIEjZHitr5PD6mx2KuQ1J4k0wyU1LjXacph6oXivdunEU3x7FytGhlQEZ-EbY4KMfskmRFaWnZiYcuF8bXc3woj4MnT2OQNm6xkBQ"
-			const creatable = payfunc.Account.is(account) &&
-				authly.Token.is(account.method[account.method.length - 1].token) && {
-					items: 13.37,
-					currency: "SEK",
-					payment: {
-						type: "account",
-						token: account.method[account.method.length - 1].token,
-					},
-				}
+			// This test will fail locally. This is a legacy test with a token that isn't possible to create anymore (but may still be used by old merchants). If cardfunc card (testtest user) with id: "wLSTzEra" is removed this test will fail on all servers.
+			const accountToken =
+				"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwYXlmdW5jIiwiaWF0IjoxNjAwNzc5NTg4NzM2LCJhdWQiOiJwcm9kdWN0aW9uIiwicmVmZXJlbmNlIjoiZXlKaGJHY2lPaUpTVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBjM01pT2lKRFlYSmtSblZ1WXlJc0ltbGhkQ0k2TVRZd01EYzNNemt6TnpjM01Dd2lZWFZrSWpvaWNISnZaSFZqZEdsdmJpSXNJbWxrSWpvaWQweFRWSHBGY21FaWZRLlRNNm53c3c2ckp6aVpFMEVWZ0xzZlJNbFNqZ1BCMmZOcDlySUVLbUw5YU1oWHZ2bFR2X0J2T3NhQlFtMzVndHk4WEU2MGphWjNzQW9GUjNCVUI3dC1BeXY3LXV5WE91VU54YUF5V21IUVZWTC1BdnJFWkJBOEFISXRsUkJhOU9TdzZhd3pVYjZkaFExVkx4Z0d5RVdrQ2dHMjZNSm8xTzlKYzd2a1FhOVg0b3Bveml4UUdZS1RCOFJHam9PYlhKY1B0ektQb0xoUE5CNlZpNFBvaEFQMEdEMnpyZ19uY2t6U2FmZHAzRWNDa1dfMXNoMHVEWG05TWNsN3BQallNNG1meDFwSTR0NDB0MzVBMFVEYlVNRXJPcTUwVXBpV0hYcWFsWE9rOEV1akNTTGpkNnpkZlpRUXozeERhbGE0R045WXpidlhTajZTMkVKTWNJY2pHZTctQSIsInR5cGUiOiJjYXJkIiwic2NoZW1lIjoidmlzYSIsImlpbiI6IjQxMTExMSIsImxhc3Q0IjoiMTExMSIsImV4cGlyZXMiOlsyLDIyXSwiY3JlYXRlZCI6IjIwMjAtMDktMjJUMTI6NTk6NDguNjY5WiJ9.q0urLPlkr84tLvBJsPze2Nb1mLcJ6qgOHrNBXveFZlXDWeKTU0XoxTN5uRBWWq8RvCysCiVTaBVznPfpKvrNQGLPGBxRo0y-xEx2ZLTMuJnMsqco-tNA8seO9HwAY5QOapIcSAlZqcNvHrEOWg62hXhrnUWZgAmgVrpgg5bQtp_ZH5SVdZdazB-1fZAMpChJkkyDJLDhO-T7ilNpDHV-AeUYs-WH6ulITIqGWF1tse4bRMYtoAIEjZHitr5PD6mx2KuQ1J4k0wyU1LjXacph6oXivdunEU3x7FytGhlQEZ-EbY4KMfskmRFaWnZiYcuF8bXc3woj4MnT2OQNm6xkBQ"
+			const creatable = {
+				items: 13.37,
+				currency: "SEK",
+				payment: {
+					type: "account",
+					token: accountToken,
+				},
+			}
 			const token =
 				connection && payfunc.Order.Creatable.is(creatable) && (await Order.create(connection, creatable, true))
 			let result: boolean
