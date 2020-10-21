51772eeae2ebac4d47a824ed8780f081aaf2658c
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-10-21 22:22:57 +0200

    bump model and key compatibility (#26)

--- Card/Connection.ts
+++ Card/Connection.ts
@@ -2,6 +2,8 @@ import * as cardfunc from "@cardfunc/cli"
 import * as payfunc from "@payfunc/model"
 
 export async function convert(connection: cardfunc.Connection): Promise<cardfunc.Connection | undefined> {
-	const payfuncPublic = await payfunc.Merchant.Key.KeyInfo.unpack(connection?.credentials?.keys.public, "public")
+	const payfuncPublic = connection?.credentials?.keys.public
+		? await payfunc.Key.unpack(connection?.credentials?.keys.public, "public")
+		: undefined
 	return (payfuncPublic && (await connection.change({ url: payfuncPublic?.card?.url }))) || undefined
 }
